db1231c750156c4ed9c1f7729abd3de46df473ff
YU <96750543+yhuj79@users.noreply.github.com>
2022-10-29 05:50:34 +0900

    [Bronze IV] Title: 주사위 세개, Time: 140 ms, Memory: 9340 KB -BaekjoonHub

--- 백준/Bronze/2480. 주사위 세개/주사위 세개.js
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.js
@@ -6,26 +6,14 @@ let a = parseInt(input[0]);
 let b = parseInt(input[1]);
 let c = parseInt(input[2]);
 
-if (a === b && a === c) {
+if (a == b && b == c) {
   console.log(10000 + a * 1000);
-} else if (a === b && a != c) {
-  console.log(1000 + a * 100);
-} else if (a != b && b === c) {
-  console.log(1000 + b * 100);
-} else if (a === c && a != b) {
-  console.log(1000 + a * 100);
-} else {
-  if (a > b && a > c) {
-    console.log(a * 100);
-  } else if (a > c && a > b) {
-    console.log(a * 100);
-  } else if (b > a && b > c) {
-    console.log(b * 100);
-  } else if (b > c && b > a) {
-    console.log(b * 100);
-  } else if (c > a && c > b) {
-    console.log(c * 100);
-  } else if (c > b && c > a) {
-    console.log(c * 100);
+} else if (a == b || b == c || a == c) {
+  if (a == b) {
+    console.log(1000 + a * 100);
+  } else {
+    console.log(1000 + c * 100);
   }
+} else {
+  console.log(Math.max(a, b, c) * 100);
 }
