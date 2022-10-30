ef145001d1796988b61db547036c45a0f3a07d1c
YU <96750543+yhuj79@users.noreply.github.com>
2022-10-30 11:37:57 +0900

    [Bronze V] Title: A+B - 4, Time: 132 ms, Memory: 9576 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.js
@@ -0,0 +1,8 @@
+let fs = require("fs");
+
+let input = fs.readFileSync("dev/stdin").toString().trim().split("\n");
+
+for (let i = 0; i <= input.length - 1; i++) {
+  let num = input[i].split(" ");
+  console.log(Number(num[0]) + Number(num[1]));
+}
