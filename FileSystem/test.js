3dbb46872f212aa5994737bfdba243874bd5591e
yhuj79 <yu1085@naver.com>
2022-11-29 00:53:07 +0900

    fs

--- FileSystem/test.js
+++ FileSystem/test.js
@@ -1,16 +1,3 @@
-let fs = require("fs");
-
-let input = fs.readFileSync("ex.txt").toString().trim().split("\n");
-
-let x = parseInt(input[0]);
-let y = parseInt(input[1]);
-
-if (x > 0 && y > 0) {
-  console.log("1");
-} else if (x < 0 && y > 0) {
-  console.log("2");
-} else if (x < 0 && y < 0) {
-  console.log("3");
-} else if (x > 0 && y < 0) {
-  console.log("4");
-}
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+let input = fs.readFileSync(filePath).toString().trim();
