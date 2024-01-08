1fb8fa9f2c719f860165553650ebb6956c8ad980
YU <yhuj79@outlook.com>
2024-01-09 00:58:50 +0900

    [Bronze III] Title: 세 막대, Time: 128 ms, Memory: 9340 KB -BaekjoonHub

---
+++ 백준/Bronze/14215. 세 막대/세 막대.js
@@ -0,0 +1,9 @@
+let input = require('fs').readFileSync('/dev/stdin').toString().trim().split(' ');
+
+let [a, b, c] = input.map(n=>parseInt(n)).sort((a,b)=>a-b);
+
+if(a+b>c){
+    console.log(a+b+c)
+}else{
+    console.log(2*(a+b)-1)
+}
