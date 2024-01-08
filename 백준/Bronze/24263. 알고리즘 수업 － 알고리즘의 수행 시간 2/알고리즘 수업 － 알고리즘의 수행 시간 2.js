45fd33c0ba71a843735e90e6129830eac41c47a9
YU <yhuj79@outlook.com>
2024-01-09 01:30:24 +0900

    [Bronze IV] Title: 알고리즘 수업 - 알고리즘의 수행 시간 2, Time: 124 ms, Memory: 9332 KB -BaekjoonHub

---
+++ 백준/Bronze/24263. 알고리즘 수업 － 알고리즘의 수행 시간 2/알고리즘 수업 － 알고리즘의 수행 시간 2.js
@@ -0,0 +1,6 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+const input = fs.readFileSync(filePath).toString().trim();
+
+console.log(Number(input));
+console.log(1);
