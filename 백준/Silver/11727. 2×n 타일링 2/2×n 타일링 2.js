df01754c80391c63dcb127cdfb6826a0f22692f4
YU <yhuj79@outlook.com>
2024-08-04 18:14:42 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 148 ms, Memory: 9648 KB -BaekjoonHub

---
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.js
@@ -0,0 +1,14 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+const input = fs.readFileSync(filePath).toString().trim();
+
+const dp = {
+  1: 1,
+  2: 3,
+};
+
+for (let i = 3; i <= input; i++) {
+  dp[i] = (dp[i - 1] + dp[i - 2] * 2) % 10007;
+}
+
+console.log(dp[input]);
